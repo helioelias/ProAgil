966accb1d85819770411cb519190ec0d284b6310
Helio Elias de Moura Carvalho <helio.carvalho@unitelco.com.br>
2020-04-15 14:17:15 -0300

    arquivos webapi .net core

---
+++ ProAgil.WebAPI/Controllers/EventoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly ILogger<EventoController> _logger;
+        public DataContext Context { get; }
+
+        public EventoController(ILogger<EventoController> logger, DataContext context)
+        {
+            this.Context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+
+                var results = await Context.Eventos.ToListAsync();
+                return Ok(results);
+
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var results = await Context.Eventos.FirstOrDefaultAsync(w => w.EventoId == id);
+                return Ok(results);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+    }
+}
